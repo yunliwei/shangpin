4457ff8da7d65fabf0b973d62ff20a60c28e1c3b
yunliwei <1127429516@qq.com>
2016-05-17 17:29:08 +0800

    liwei

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,25 +1,5 @@
 class LoginsController < ApplicationController
-  def index
-    admin= Admin.all
-    if admin.count==0
-      Admin.create(use_name:'admin',password:'admin',password_confirmation:'admin')
-    end
-  end
 
-
-
-  def create
-
-
-    admin = Admin.find_by(use_name:params[:login][:use_name])
-    if admin && admin.authenticate(params[:login][:password])
-
-      redirect_to admins_path
-
-    else
-      redirect_to action: 'index',id:0
-    end
-  end
 
 
 end
